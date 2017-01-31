6255d1c804db4ab68fd99db308e76d764bad8c6e
LIRUJIA <rxl635@cs.bham.ac.uk>
2017-01-31 20:49:39 +0000

    增加跳转页面配置

--- src/main/java/com/bham/sso/controller/HotelController.java
+++ src/main/java/com/bham/sso/controller/HotelController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/hotels")
@@ -13,5 +14,11 @@ public class HotelController {
 	public String list(Model model) {
 		return "hotels/home";
 	}
+	
+    @RequestMapping("test")
+    public ModelAndView handleRequest(Model model) {
+        model.addAttribute("title", "Hello Mustache");
+        return new ModelAndView("home").addAllObjects(model.asMap());
+    }
 	
 }
